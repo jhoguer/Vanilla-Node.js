2ffbd96b2e8454519fb623bbc09c228d73f6e02f
Jhon Guerrero <jhoguer@gmail.com>
2020-11-09 10:43:29 -0500

    Manejando la ruta GET basica

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const products = require('./data/products.json');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<h1>Hello World</h1>');
-  res.end();
+  if (req.url === '/api/products' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route Nor Found'}));
+  }
 });
+
 
 const PORT = process.env.PORT || 5000;
 
