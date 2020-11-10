e833ffd70820f020f8ac8cbdec5aee99bc0d12e4
Jhon Guerrero <jhoguer@gmail.com>
2020-11-10 00:37:12 -0500

    Separando la logica en capas

---
+++ models/productModel.js
@@ -0,0 +1,12 @@
+const products = require('../data/products.json');
+
+
+function findAll() {
+  return new Promise( (resolve, reject) => {
+    resolve(products)
+  })
+}
+
+module.exports = {
+  findAll
+};
